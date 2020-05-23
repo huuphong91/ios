2b477060ac885995df12e2ab0491fc63fcba30e1
Huu Phong <huuphong91@gmail.com>
2020-05-23 19:12:29 +0700

    init project

---
+++ HelloMyApp/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMyApp
+//
+//  Created by Phong Toan Huu on 5/17/20.
+//  Copyright © 2020 Phong Toan Huu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '9.0'
+
+target 'HelloMyApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HelloMyApp
+  pod 'RxSwift'
+  pod 'RxSwiftExt'
+  pod 'SwiftyJSON'
+  pod 'Alamofire'
+
+end
+
+
